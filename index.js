c774215268a58156935e0719fd6a5e6189b9ab90
SohaniKajla <sohani5755@gmail.com>
2025-07-29 19:42:04 +0530

    First Message

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require("express")
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to Home Page")
+})
+app.get('/login',(req,res)=>{
+    res.send('First Login to Our Website');
+})
+app.get('/signup',(req,res)=>{
+    res.send('Sign Up Page')
+})
+
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on Port http://localhost:${PORT}`);
+    
+})
